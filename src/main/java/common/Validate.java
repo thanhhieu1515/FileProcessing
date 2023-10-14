acedbd132cdf5e1a72b7b20e992aaea2fa259eb5
thanhhieu1515 <144198175+thanhhieu1515@users.noreply.github.com>
2023-10-14 12:34:36 +0700

    Add files via upload

---
+++ src/main/java/common/Validate.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package common;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author DAO
+ */
+public class Validate {
+
+    private static final Scanner in = new Scanner(System.in);   
+    //check user input string
+    public static String checkInputPathFile() {
+        //loop until user input correct
+        while (true) {
+            String result = in.nextLine().trim();
+            if (result.isEmpty()) {
+                System.err.println("Not empty");
+                System.out.print("Enter again: ");
+            } else {
+                return result;
+            }
+        }
+    }
+    
+    //check user input double
+    public static double checkInputMoney() {
+        //loop until user input corect
+        System.out.print("Enter money: ");
+        while (true) {
+            try {
+                double result = Double.parseDouble(in.nextLine());
+                return result;
+            } catch (NumberFormatException e) {
+                System.err.println("Must be input double");
+                System.out.print("Enter again: ");
+            }
+        }
+    }
+}
